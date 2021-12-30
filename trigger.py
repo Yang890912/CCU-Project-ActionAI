33359df5258b77f74d67272335cb2f7edf251fe1
yang <nyy85918591@gmail.com>
2021-12-30 17:34:26 +0800

    add trigger.py

---
+++ trigger.py
@@ -0,0 +1,7 @@
+from Capturevediotoimage import VedioConverter 
+
+
+
+if __name__ == '__main__':
+    test = VedioConverter()
+    test.test_predict("D:/文件/python_programming/project/train_images/production ID_4271760.mp4", 15, 120)
