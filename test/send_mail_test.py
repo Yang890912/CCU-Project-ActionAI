23ed922b33a811e61a921fe0b2510d913d03c5e0
ChinYikMing <yikming2222@gmail.com>
2022-01-03 19:34:21 +0800

    email test

--- test/send_mail_test.py
+++ test/send_mail_test.py
@@ -1,30 +1,53 @@
+import re
 import unittest
 import sys
 sys.path.append('../')
 
 from SendGmail import SendGmail
+from Capturevediotoimage import VedioConverter
 
 def usage(script_name):
-    print("python %s threshold email_recv" % script_name)
+    print("python %s video_to_test_path rest_skiptime work_skiptime threshold email_recv" % script_name)
 
-if len(sys.argv) != 3:
+def email_validate(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.fullmatch(regex, email)
+
+if len(sys.argv) != 6:
     usage(sys.argv[0])
     exit()
 
+if not email_validate(sys.argv[5]):
+    print("Invalid receiver email address")
+    exit()
+
 class TestSendMail(unittest.TestCase):
+    video_to_test = None
+    rest_skiptime = None
+    work_skiptime = None
     thres = None
     recver = None
     snder = None
+    predictor = None
 
     def setUp(self):
-        self.thres = int(sys.argv[1])
-        self.recver = sys.argv[2]
-        self.snder = SendGmail("test88812345@gmail.com", "1234567a.", self.recver)
+        self.predictor = VedioConverter()
+        self.video_to_test = sys.argv[1]
+        self.rest_skiptime = int(sys.argv[2])
+        self.work_skiptime = int(sys.argv[3])
+        self.thres = int(sys.argv[4])
+        self.recver = sys.argv[5]
+        self.snder = SendGmail("your_email", "your_email_password", self.recver)
 
     # 當 model 預測工作超時的時候，發送 email
     def test_send_mail_with_thres(self):
-        is_failed = self.snder.send_message()
-        self.assertFalse(is_failed)
+        worktime, videotime = self.predictor.test_predict(self.video_to_test, self.rest_skiptime, self.work_skiptime)
+        try: 
+            self.assertGreaterEqual(worktime, self.thres)
+            is_failed = self.snder.send_message()
+            self.assertFalse(is_failed)
+        except AssertionError as e: 
+            print("No reach the threshold")
 
 if __name__ == '__main__':
     unittest.main(argv=['ignored', '-v'], exit=True)
